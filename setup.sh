e0294680c18f8689d5a47f85f6c2130bb729375a
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-04-07 00:33:42 +0900

    chore: Add .asdfrc

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
-DOT_FILES=(.bashrc .zshrc .gitconfig)
+DOT_FILES=(.bashrc .zshrc .gitconfig .asdfrc)
 
 for file in ${DOT_FILES[@]}
 
