1c3bd9f9e2e133accff962312796c454e5300179
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-12-19 12:28:01 +0900

    :wrench: force symlink dotfiles

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# . brew.sh
-# . dock.sh
+. brew.sh
+. dock.sh
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
@@ -10,7 +10,7 @@ DOT_FILES=(.bashrc .zshrc .gitconfig .asdfrc .hushlogin)
 for file in ${DOT_FILES[@]}
 
 do
-	ln -s $HOME/dotfiles/$file $HOME/$file
+	ln -sf $HOME/dotfiles/$file $HOME/$file
 	echo "シンボリックリンクを貼りました: $file"
 done
 
